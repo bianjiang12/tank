50ffcb18ad2ec81521eb3691b46314446d015c30
bianjiang12 <125916435@qq.com>
2020-06-29 22:09:16 +0800

    添加坦克静止的处理，只要上下左右键没按下去，坦克就是静止的

--- src/main/java/com/bianjiang/tank/Tank.java
+++ src/main/java/com/bianjiang/tank/Tank.java
@@ -11,6 +11,16 @@ public class Tank {
 
     private static final int SPEED = 10;
 
+    private boolean moving = false;
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
     public Tank(int x, int y, Dir dir) {
         this.x = x;
         this.y = y;
@@ -27,6 +37,12 @@ public class Tank {
 
     public void paint(Graphics g) {
         g.fillRect(x, y, 50, 50);
+        move();
+    }
+
+    private void move() {
+        if (!moving) return;
+
         switch (dir) {
             case LEFT:
                 x -= SPEED;
