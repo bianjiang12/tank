7e91df371e10a57a7fb5ca44d3516bd4eeac27a7
bianjiang12 <125916435@qq.com>
2020-07-01 22:36:56 +0800

    在main方法添加背景音乐

--- src/main/java/com/bianjiang/tank/Main.java
+++ src/main/java/com/bianjiang/tank/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame frame = new TankFrame();
 
+        new Thread(()->new Audio("audio/war1.wav").loop()).start();
+
         //初始化敌方坦克
         for (int i = 0; i < 5; i++) {
             frame.tanks.add(new Tank(50 + i * 80, 200, Dir.DOWN, Group.BAD, frame));
